12932824f89a45b477e032e39a8147d61f06306c
DrifterEX <134128987+DrifterEX@users.noreply.github.com>
2023-05-20 23:08:33 +0800

    Delete SyrexDB directory

--- SyrexDB/ReminderList.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[ReminderList]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(50) NOT NULL, 
-    [Description] NVARCHAR(MAX) NULL, 
-    [ReminderDate] DATETIME NULL, 
-    [Status] INT NOT NULL
-)
